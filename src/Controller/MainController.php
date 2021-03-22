7dbbf1206bcdebc9acfc40e2fdf5ada35b74e4f6
Jarousseau_Herve <jarousseau.herve@gmail.com>
2021-03-22 14:35:08 +0100

    creation main controller (HJ)

---
+++ src/Controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="main_home")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function home() {
+        return $this->render('main/home.html.twig');
+    }
+
+
+
+}
